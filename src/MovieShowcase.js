d6bf5d92998cf4b7a80e2d3afaea95754544a2cc
Alex Waller <alex@artofalexwaller.com>
2021-04-28 20:25:59 -0400

    commit tree 70314d0a22d8273302853790249a06c8f5fc5fd3

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,7 +6,14 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
-  }
+    return movieData.map((movie, index) => <MovieCard
+    key={index}
+    title={movie.title}
+    IMDBRating={movie.IMDBRating}
+    genres={movie.genres}
+    poster={movie.poster}
+    />)
+  };
 
   render() {
     return (
@@ -16,3 +23,7 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+
+// the component that will house all of the 'raw' data associated 
+// with the movies we want to display
+// passes on data to child component MovieCard
